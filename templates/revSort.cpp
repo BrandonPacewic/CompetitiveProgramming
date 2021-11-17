1adca8609956e92abc070b573e2dc85656181b73
Brandon <pacewicbrandon@gmail.com>
2021-11-17 15:12:58 -0800

    update

--- templates/revSort.cpp
+++ templates/revSort.cpp
@@ -2,15 +2,9 @@
 using namespace std;
 
 vector<int> revSort(vector<int> list) {
-    for (int i = 1; i <= list.size();) {
-        for (int k = i; k < list.size(); k++) 
-            if (list[k] == i) {
-                reverse(list.begin() + i - 1, list.begin() + k + 1);
-                goto start;
-            }
-        
-        start:
-            i++;
+    for (int i = 0; i < list.size() - 1; i++) {
+        int index = int(min_element(list.begin() + i, list.end()) - list.begin());
+        reverse(list.begin() + i, list.begin() + index + 1);
     }
     return list;
 }
