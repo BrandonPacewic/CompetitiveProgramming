e534badb969f2266881d0fe91fbe186e6cd92482
Brandon <pacewicbrandon@gmail.com>
2021-11-18 09:03:13 -0800

    update

--- templates/dbg.cpp
+++ templates/dbg.cpp
@@ -5,10 +5,10 @@ using namespace std;
 const bool DBG_MODE = false; 
 long long dbg_count = 0ll;
 template<typename Ints> void testList(Ints List) { if (DBG_MODE) { cerr << '#' << dbg_count << " __LIST_ARGS__: ("; dbg_count += 1ll; for (int i: List) { cout << ' ' << i; } cerr << " )" << endl; } }
-void dbg_out1() { cerr << endl; dbg_count += 1ll; }
-template<typename Front, typename... Back> void dbg_out1(Front K, Back... T) { cerr << ' ' << K; dbg_out1(T...); }
+void DBG_OUT() { cerr << endl; dbg_count += 1ll; }
+template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
 #if DBG_MODE
-#define testArgs(...) cerr << '#' << dbg_count << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", dbg_out1(__VA_ARGS__)
+#define testArgs(...) cerr << '#' << dbg_count << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", DBG_OUT(__VA_ARGS__)
 #else
 #define testArgs (...)
 #endif
--- templates/fbHackerGroup.cpp
+++ templates/fbHackerGroup.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 //dbg
+const bool DBG_MODE = false; 
 long long dbg_count = 0ll;
-void dbg_out1() { cerr << endl; dbg_count += 1ll; }
-template<typename Front, typename... Back> void dbg_out1(Front K, Back... T) { cerr << ' ' << K; dbg_out1(T...); }
-#define testArgs(...) cerr << '#' << dbg_count << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", dbg_out1(__VA_ARGS__)
-
-const string data_type[1] = { "St6vectorIiSaIiEE" };
-void assertListType() { cerr << '#' << dbg_count << " __LIST_ARGS__: ("; dbg_count += 1ll; }
-bool findType(string type) { for (auto i: data_type) { if (i == type) return true; } return false; }
-template<typename Arr> void testListInt(Arr List) { assert(findType(typeid(List).name())); assertListType(); for (int i: List) { cout << ' ' << i; } cerr << " )" << endl; }
+template<typename Ints> void testList(Ints List) { if (DBG_MODE) { cerr << '#' << dbg_count << " __LIST_ARGS__: ("; dbg_count += 1ll; for (int i: List) { cout << ' ' << i; } cerr << " )" << endl; } }
+void DBG_OUT() { cerr << endl; dbg_count += 1ll; }
+template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
+#if DBG_MODE
+#define testArgs(...) cerr << '#' << dbg_count << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", DBG_OUT(__VA_ARGS__)
+#else
+#define testArgs (...)
+#endif
 
 
 void runCase(int tc) {
@@ -20,11 +21,11 @@ void runCase(int tc) {
 }
 
 int main() {
-    int tc;
-    cin >> tc;
+    int test_cases;
+    cin >> test_cases;
 
-    for (int case_num = 1; case_num <= tc; case_num++) {
-        runCase(case_num);
+    for (int tc = 1; test_cases <= tc; tc++) {
+        runCase(tc);
         cerr << flush;
     }
 }
