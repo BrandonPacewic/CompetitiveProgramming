2f1f2931cea7dc7638c3fcf5dce5ccc34f31fb9c
Brandon <pacewicbrandon@gmail.com>
2021-11-18 09:04:24 -0800

    update

--- templates/dbg.cpp
+++ templates/dbg.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //dbg
 const bool DBG_MODE = false; 
 long long dbg_count = 0ll;
-template<typename Ints> void testList(Ints List) { if (DBG_MODE) { cerr << '#' << dbg_count << " __LIST_ARGS__: ("; dbg_count += 1ll; for (int i: List) { cout << ' ' << i; } cerr << " )" << endl; } }
+template<typename Ints> void testList(Ints List) { if (!DBG_MODE) return; cerr << '#' << dbg_count << " __LIST_ARGS__: ("; dbg_count += 1ll; for (int i: List) { cout << ' ' << i; } cerr << " )" << endl; }
 void DBG_OUT() { cerr << endl; dbg_count += 1ll; }
 template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
 #if DBG_MODE
