639b5ff8535536db022f31fb424a5aa1da379581
Brandon <pacewicbrandon@gmail.com>
2021-11-18 08:57:48 -0800

    update

--- templates/dbg.cpp
+++ templates/dbg.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 //dbg
+const bool DBG_MODE = false; 
 long long dbg_count = 0ll;
+template<typename Ints> void testList(Ints List) { if (DBG_MODE) { cerr << '#' << dbg_count << " __LIST_ARGS__: ("; dbg_count += 1ll; for (int i: List) { cout << ' ' << i; } cerr << " )" << endl; } }
 void dbg_out1() { cerr << endl; dbg_count += 1ll; }
 template<typename Front, typename... Back> void dbg_out1(Front K, Back... T) { cerr << ' ' << K; dbg_out1(T...); }
+#if DBG_MODE
 #define testArgs(...) cerr << '#' << dbg_count << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", dbg_out1(__VA_ARGS__)
-
-const string data_type[1] = { "St6vectorIiSaIiEE" };
-void assertListType() { cerr << '#' << dbg_count << " __LIST_ARGS__: ("; dbg_count += 1ll; }
-bool findType(string type) { for (auto i: data_type) { if (i == type) return true; } return false; }
-template<typename Arr> void testListInt(Arr List) { assert(findType(typeid(List).name())); assertListType(); for (int i: List) { cout << ' ' << i; } cerr << " )" << endl; }
+#else
+#define testArgs (...)
+#endif
