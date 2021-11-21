c4228a9b3545eea585badfd7bc32317fc8336de2
Brandon <pacewicbrandon@gmail.com>
2021-11-20 17:26:14 -0800

    inital commit

--- templates/printPairs.hpp
+++
@@ -1,5 +0,0 @@
-template<typename T_Pairs>
-void printPairs(T_Pairs pairs) {
-    for (int i = 0; i < pairs.size(); i++) 
-        cout << "pair# " << i << ": " << pairs[i].first << ' ' << pairs[i].second << '\n';  
-}   
