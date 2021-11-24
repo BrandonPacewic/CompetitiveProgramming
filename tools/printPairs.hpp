e66d371e9aa7a51bcf01e47300e2045244b3bc66
Brandon <pacewicbrandon@gmail.com>
2021-11-23 23:50:27 -0800

    print function update

--- tools/printPairs.hpp
+++ tools/printPairs.hpp
@@ -1,5 +1,16 @@
 template<typename T_Pairs>
-void printPairs(T_Pairs pairs) {
-    for (int i = 0; i < pairs.size(); i++) 
-        cout << "pair# " << i << ": " << pairs[i].first << ' ' << pairs[i].second << '\n';  
+void printPairs(T_Pairs pairs, bool pairSpacing = true, bool newPairSpace = true, bool newLine = true) {
+    for (int i = 0; i < pairs.size(); i++) {
+        cout << pairs[i].first;
+
+        if (pairSpacing)
+            cout << ' ';
+
+        cout << pairs[i].second;
+
+        cout << (newPairSpace ? ' ' : '\n');
+    }
+
+    if (newLine)
+        cout << '\n';
 }   
