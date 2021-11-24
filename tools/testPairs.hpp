2715bfb02338ba2d24600aa9aad9fdf8c22bdc2d
Brandon <pacewicbrandon@gmail.com>
2021-11-24 11:40:00 -0800

    inital commit

---
+++ tools/testPairs.hpp
@@ -0,0 +1,6 @@
+//dbg pairs
+#ifdef DBG_MODE
+template<typename T_Pairs> void testPairs(T_Pairs Pairs) { cerr << '#' << DBG_COUNT << " __PAIR_ARGS__: ("; DBG_CONUT++; for (int i = 0; i < Pairs.size(); i++) { cout << Pairs[i].first << '-' << Pairs[i].second << (i < Pairs.size() - 1 ? ", " : ")\n"); } } 
+#else
+template<typename T_Pairs> void testPairs(T_Pairs Pairs) { return; }
+#endif
