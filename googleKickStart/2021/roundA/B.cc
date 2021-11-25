3367a34e7508318b53efdf217a9112a2d36df56f
Brandon <pacewicbrandon@gmail.com>
2021-11-25 14:27:18 -0800

    inital commit

---
+++ googleKickStart/2021/roundA/B.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//dbg
+#define DBG_MODE
+int64_t DBG_COUNT = 0;
+void DBG_OUT() { cerr << endl; DBG_COUNT++; }
+template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
+#ifdef DBG_MODE
+template<typename T_Ints> void testList(T_Ints List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__: ("; DBG_COUNT++; for (int i = 0; i < List.size(); i++) { cout << List[i] << (i < List.size() - 1 ? ", " : ")\n"); } }
+#define testArgs(...) cerr << '#' << DBG_COUNT << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", DBG_OUT(__VA_ARGS__)
+#else
+template<typename T_Ints> void testList(T_Ints List) { return; }
+#define testArgs(...)
+#endif
+
+
+void runCase(int tc) {
+    //case code here
+
+    cout << "Case #" << tc << ": " << '\n';
+}
+
+int main() {
+    int test_cases;
+    cin >> test_cases;
+
+    for (int tc = 1; tc <= test_cases; tc++) {
+        runCase(tc);
+        cerr << flush;
+    }
+}
