65e31fcc4e9f53c861065707829db043df6b5821
Brandon <pacewicbrandon@gmail.com>
2021-11-25 23:06:43 -0800

    still not working

--- googleKickStart/2021/roundA/B.cc
+++ googleKickStart/2021/roundA/B.cc
@@ -20,23 +20,18 @@ bool checkEdge(vector<vector<int>> M, pair<int, int> L) {
         return 0;
 
     if (L.first - 1 < 0 && M[L.first + 1][L.second]) {
-        testArgs("this");
         return 1;
     } else if (L.first + 1 > M.size() && M[L.first - 1][L.second]) {
-        testArgs("one");
         return 1;
     } else if (L.second - 1 < 0 && M[L.first][L.second + 1]) {
-        testArgs("is");
         return 1;
     } else if (L.second + 1 > M[0].size() && M[L.first][L.second - 1]) {
-        testArgs("the");
         return 1;
-    } else if ((M[L.first - 1][L.second] != M[L.first + 1][L.second]) || (M[L.first][L.second - 1] != M[L.first][L.second + 1])) {
-        testArgs("best");
+    } 
+    
+    if ((L.first - 1 < 0 || L.first + 1 > M.size() || L.second - 1 < 0 || L.second + 1 > M[0].size()) && (M[L.first - 1][L.second] != M[L.first + 1][L.second]) || (M[L.first][L.second - 1] != M[L.first][L.second + 1])) 
         return 1;
-    }
-
-    testArgs("again");
+    
     return 0;
 }
 
@@ -55,45 +50,52 @@ void runCase(int tc) {
         for (int k = 0; k < C; k++) {
             if(checkEdge(M, pair<int, int> (i, k))) {
                 int up = 0, dw = 0, le = 0, ri = 0;
+                testArgs(i, k);
 
-                // for (int j = 0; i - j < 0; j++) {
-                //     if (M[i - j][k]) 
-                //         up++;
-                //     else
-                //         break;
-                // }
+                for (int j = 0; i - j >= 0; j++) {
+                    //testargs(j);
+                    if (M[i - j][k]) 
+                        up++;
+                    else
+                        break;
+                }
+
+                for (int j = 0; i + j <= R - 1; j++) {
+                    //testargs(j);
+                    if (M[i + j][k])
+                        dw++;
+                    else 
+                        break;
+                }
+
+                for (int j = 0; k - j >= 0; j++) {
+                    //testargs(j);
+                    if (M[i][k - j])
+                        le++;
+                    else
+                        break;
+                }
+
+                for (int j = 0; k + j <= C - 1; j++) {
+                    //testargs(j);
+                    if (M[i][k + j])
+                        ri++;
+                    else
+                        break;
+                }
+
+                testArgs(up, dw, le, ri);
+
+                if (up <= le / 2 && up != 1) count++; 
+                if (up <= ri / 2 && up != 1) count++; 
+                if (dw <= le / 2 && dw != 1) count++; 
+                if (dw <= ri / 2 && dw != 1) count++; 
+                if (le <= up / 2 && le != 1) count++; 
+                if (le <= dw / 2 && le != 1) count++; 
+                if (ri <= up / 2 && ri != 1) count++; 
+                if (ri <= dw / 2 && ri != 1) count++;
                 
-                // for (int j = 0; i + j > R - 1; j++) {
-                //     if (M[i + j][k])
-                //         dw++;
-                //     else 
-                //         break;
-                // }
-
-                // for (int j = 0; k - j < 0; j++) {
-                //     if (M[i][k - j])
-                //         le++;
-                //     else
-                //         break;
-                // }
-
-                // for (int j = 0; k + j > C - 1; j++) {
-                //     if (M[i][k + j])
-                //         ri++;
-                //     else
-                //         break;
-                // }
-
-                // if (up >= le / 2) count++; 
-                // if (up >= ri / 2) count++; 
-                // if (dw >= le / 2) count++; 
-                // if (dw >= ri / 2) count++; 
-                // if (le >= up / 2) count++; 
-                // if (le >= dw / 2) count++; 
-                // if (ri >= up / 2) count++; 
-                // if (ri >= dw / 2) count++;
-                
-                // testArgs(count);
+                testArgs(count);
             }
         }
     }
