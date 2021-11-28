507c9d38a240581a93d86d9bdc607956ca137379
Brandon <pacewicbrandon@gmail.com>
2021-11-27 22:02:18 -0800

    added in/out txt option

--- templates/googleCodeJam.cc
+++ templates/googleCodeJam.cc
@@ -7,26 +7,38 @@ int64_t DBG_COUNT = 0;
 void DBG_OUT() { cerr << endl; DBG_COUNT++; }
 template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
 #ifdef DBG_MODE
-template<typename T_Ints> void testList(T_Ints List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__: ("; DBG_COUNT++; for (int i = 0; i < List.size(); i++) { cout << List[i] << (i < List.size() - 1 ? ", " : ")\n"); } }
+template<typename T_List> void testList(T_List List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__: ("; DBG_COUNT++; for (int i = 0; i < List.size(); i++) { cout << List[i] << (i < List.size() - 1 ? ", " : ")\n"); } }
 #define testArgs(...) cerr << '#' << DBG_COUNT << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", DBG_OUT(__VA_ARGS__)
 #else
-template<typename T_Ints> void testList(T_Ints List) { return; }
+template<typename T_List> void testList(T_List List) { return; }
 #define testArgs(...)
 #endif
 
 
-void runCase(int tc) {
+void runCase() {
     //case code here
 
-    cout << "Case #" << tc << ": " << '\n';
+
 }
 
+
+#define TEXT_IO
 int main() {
+    #ifdef TEXT_IO
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    printf("Don't Forget to Submit Without DBG Enabled\n\n");
+    #endif
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int test_cases;
     cin >> test_cases;
 
     for (int tc = 1; tc <= test_cases; tc++) {
-        runCase(tc);
+        cout << "Case #" << tc << ": ";
+        runCase();
         cerr << flush;
     }
 }
