9ac92d7beb28e5e3106504c3bfdd2aed9e4f8709
Brandon <pacewicbrandon@gmail.com>
2021-11-28 14:21:49 -0800

    added ability to compile in console

---
+++ python_dbrun/compileInConsole.py
@@ -0,0 +1,48 @@
+#! /usr/local/bin/python
+
+# python dbrun -i <filename> (without .cc extension)
+import sys, os, getopt
+
+
+def enterDbgMode(file_name):
+    file = open(file_name, 'r')
+    lines = file.readlines()
+    lines[3] = '#define DBG_MODE' + '\n'
+
+    file = open(file_name, 'w')
+    file.writelines(lines)
+    file.close()
+
+def exitDbgMode(file_name):
+    file = open(file_name, 'r')
+    lines = file.readlines()
+    lines[3] = '//dbg' + '\n'
+
+    file = open(file_name, 'w')
+    file.writelines(lines)
+    file.close()
+
+
+def main(argv):
+    opts, args = getopt.getopt(argv, "hi:",["help",'ifile='])
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            print('run_cpp.py -i <filename> (without .cc extension)')
+            sys.exit()
+        
+        elif o in ("-i", "ifile"):
+            cc_file = a + '.cc'
+            enterDbgMode(cc_file)
+            run(cc_file)
+            exitDbgMode(cc_file)
+
+
+def run(cpp_file):
+    os.system("echo Compiling " + cpp_file + " with g++ 17")
+    os.system('g++ ' + cpp_file)
+    os.system("echo Running " + cpp_file)
+    os.system("echo -------------------")
+    os.system('./a.out')
+
+if __name__=='__main__':
+    main(sys.argv[1:])
