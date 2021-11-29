7042516001155cf3dbf549243ce733e9ae5ef22d
Brandon <pacewicbrandon@gmail.com>
2021-11-29 15:47:14 -0800

    why no work?

--- googleKickStart/2020/roundA/B.cc
+++ googleKickStart/2020/roundA/B.cc
@@ -19,10 +19,13 @@ const int INF = int(1e9) + 5;
 template<typename T_Grid> 
 int maxOfGrid(T_Grid Grid) {
     int max_val = -INF;
-    for (int i = 0; i < Grid.size(); i++)
+    for (int i = 0; i < Grid.size(); i++) {
+        testList(Grid[i]);
+
         for (int j = 0; j < Grid[i].size(); j++)
             max_val = max(max_val, Grid[i][j]);
     
+    }
     return max_val;
 }
 
