ec797b430fd4f9b2c9a211b0f0e599c39b4f3e05
Brandon <pacewicbrandon@gmail.com>
2021-11-30 16:45:54 -0800

    changed t-list to const &

--- tools/printList.hpp
+++ tools/printList.hpp
@@ -1,11 +1,10 @@
 template<typename T_List>
-void printList(T_List List, bool space = true, bool new_line = true) {
+void printList(const T_List &List, const bool space = true, const bool new_line = true) {
 
     for (int i = 0; i < List.size(); i++) {
         cout << List[i];
         
-        if (space)
-            cout << ' ';
+        if (space) cout << ' ';
     }    
         if (new_line)
             cout << '\n';
