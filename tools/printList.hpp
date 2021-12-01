17e0ce2736fa06f0f48b125220865b87109e367a
Brandon <pacewicbrandon@gmail.com>
2021-11-30 16:52:58 -0800

    added const and & to typename

--- tools/printList.hpp
+++ tools/printList.hpp
@@ -1,6 +1,5 @@
 template<typename T_List>
 void printList(const T_List &List, const bool space = true, const bool new_line = true) {
-
     for (int i = 0; i < List.size(); i++) {
         cout << List[i];
         
