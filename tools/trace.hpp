b04c3b6ffc3688c8f193f2fadb36f9d381d7782f
Brandon <pacewicbrandon@gmail.com>
2021-11-30 16:47:33 -0800

    changed formatting and changed t to const t &

--- tools/trace.hpp
+++ tools/trace.hpp
@@ -1,5 +1,7 @@
-template<typename T_Matrix> int64_t trace(T_Matrix Matrix) {
+template<typename T_Matrix> 
+int64_t trace(const T_Matrix &Matrix) {
         int64_t total = 0;
+        
         for (int64_t i = 0; i < Matrix.size(); i++)
             total += int64_t(Matrix[i][i]);
         
