76a1559561dea64a83c1aeb4436142ed6e417eec
Brandon <pacewicbrandon@gmail.com>
2021-11-30 16:52:34 -0800

    added const and & to typename

--- tools/printMatrix.hpp
+++ tools/printMatrix.hpp
@@ -1,5 +1,5 @@
 template<typename T_Matrix>
-void printMatrix(T_Matrix MATRIX) {
+void printMatrix(const T_Matrix &MATRIX) {
     for (int i = 0; i < MATRIX.size(); i++)
         for (int j = 0; j < MATRIX[i].size(); j++)
             cout << MATRIX[i][j] << (j < MATRIX[i].size() - 1 ? ' ' : '\n'); 
