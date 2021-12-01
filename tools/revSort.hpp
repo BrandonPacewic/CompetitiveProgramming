12a21a35d8c193c62fe13e5925e7581aae548a2e
Brandon <pacewicbrandon@gmail.com>
2021-11-30 16:50:18 -0800

    added typename and changed type to void

--- tools/revSort.hpp
+++ tools/revSort.hpp
@@ -1,7 +1,7 @@
-vector<int> revSort(vector<int> list) {
+template<typename T_Vector>
+void revSort(T_Vector &list) {
     for (int i = 0; i < list.size() - 1; i++) {
         int index = int(min_element(list.begin() + i, list.end()) - list.begin());
         reverse(list.begin() + i, list.begin() + index + 1);
     }
-    return list;
 }
