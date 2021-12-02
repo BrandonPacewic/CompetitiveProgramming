38869ab346486bbacaad6f4ff1ea97da082e0203
Brandon <pacewicbrandon@gmail.com>
2021-12-02 11:49:19 -0800

    inital commit

--- googleKickStart/2021/roundB/B.cc
+++ googleKickStart/2021/roundB/B.cc
@@ -17,10 +17,10 @@
 using namespace std;
 
 //dbg
+#ifdef DBG_MODE
 int64_t DBG_COUNT = 0;
 void DBG_OUT() { cerr << endl; DBG_COUNT++; }
 template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
-#ifdef DBG_MODE
 template<typename T_List> void testList(T_List List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__: ("; DBG_COUNT++; for (int i = 0; i < List.size(); i++) { cerr << List[i] << (i < List.size() - 1 ? ", " : ")\n"); } }
 #define testArgs(...) cerr << '#' << DBG_COUNT << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", DBG_OUT(__VA_ARGS__)
 #else
@@ -30,41 +30,26 @@ template<typename T_List> void testList(T_List List) { return; }
 
 
 void runCase(int tc) {
-    int N;
-    string S;
-    cin >> N >> S;
+    //case code here
 
-    cout << "Case #" << tc << ": ";
-    for (int i = 0; i < N; i++) {
-        int count = 1;
-        
-        for (int k = i; k > 0; k--) {
-            if (S[k] - '0' > S[k - 1] - '0') {
-                testArgs(S[k] - '0', S[k - 1] - '0');
-                count++;
-            } else
-                break;
-        }
-        cout << count << ' ';
-    }
-    cout << '\n';
+    cout << "Case #" << tc << ": " << '\n';
 }
 
 
 int main() {
-    #ifdef TEXT_IO
+#ifdef TEXT_IO
     freopen("in.txt", "r", stdin);
     freopen("ou.txt", "w", stdout);
-    #endif
+#endif
 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     int test_cases;
     cin >> test_cases;
 
     for (int tc = 1; tc <= test_cases; tc++) {
         runCase(tc);
-        cerr << flush;
+        cout << flush;
     }
 }
