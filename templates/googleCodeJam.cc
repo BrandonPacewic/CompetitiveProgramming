cf11199a243c77c04841b2589acce90fe28ce55e
Brandon <pacewicbrandon@gmail.com>
2021-12-01 17:49:35 -0800

    changed bits to set #include lines

--- templates/googleCodeJam.cc
+++ templates/googleCodeJam.cc
@@ -1,4 +1,18 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
 using namespace std;
 
 //dbg
