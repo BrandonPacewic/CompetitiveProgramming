4ee1f52c08505d87d24ef4f0bdde3ec7873dba9a
Brandon <pacewicbrandon@gmail.com>
2021-12-12 12:14:16 -0800

    added link

--- extendedCmath/modulo.hpp
+++ extendedCmath/modulo.hpp
@@ -1,5 +1,4 @@
-//modulo
+//modulo http://www.cplusplus.com/forum/general/19502/
 template<typename T, typename OP> struct modulo { T bace; }; enum { mod };
 template<typename T> modulo<T, decltype(mod)> operator<(const T& front, decltype(mod)) { return  { front }; }
 int operator>(modulo<int, decltype(mod)> tail, int exponent) { int& bace = tail.bace; return bace % exponent < 0 ? (bace % exponent) + exponent : bace % exponent; }
-#define mod <mod>
