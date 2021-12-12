c7c91ae7330f58391f17f9fd87b7ef61ac1afcac
Brandon <pacewicbrandon@gmail.com>
2021-12-12 00:26:24 -0800

    accounted for over definition eror

--- tools/dbg.hpp
+++ tools/dbg.hpp
@@ -1,12 +1,14 @@
 //dbg
-#define DBG_MODE
+#ifdef DBG_MODE
+#ifndef BRANDON_DBG
+#define BRANDON_DBG
 int64_t DBG_COUNT = 0;
 void DBG_OUT() { cerr << endl; DBG_COUNT++; }
 template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
-#ifdef DBG_MODE
 template<typename T_List> void testList(T_List List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__: ("; DBG_COUNT++; for (int i = 0; i < List.size(); i++) { cerr << List[i] << (i < List.size() - 1 ? ", " : ")\n"); } }
 #define testArgs(...) cerr << '#' << DBG_COUNT << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", DBG_OUT(__VA_ARGS__)
 #else
 template<typename T_List> void testList(T_List List) { return; }
 #define testArgs(...)
 #endif 
+#endif
