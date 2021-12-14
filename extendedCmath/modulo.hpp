4c9537ead66a410b3364bea82df1e9359224f0cd
Brandon <pacewicbrandon@gmail.com>
2021-12-14 15:10:24 -0800

    added template to operator>

--- extendedCmath/modulo.hpp
+++ extendedCmath/modulo.hpp
@@ -1,4 +1,4 @@
 //modulo http://www.cplusplus.com/forum/general/19502/
 template<typename T, typename OP> struct modulo { T bace; }; enum { mod };
 template<typename T> modulo<T, decltype(mod)> operator<(const T& front, decltype(mod)) { return  { front }; }
-int operator>(modulo<int, decltype(mod)> tail, int exponent) { int& bace = tail.bace; return bace % exponent < 0 ? (bace % exponent) + exponent : bace % exponent; }
+template<typename T_IntType> T_IntType operator>(modulo<T_IntType, decltype(mod)> tail, T_IntType exponent) { T_IntType& bace = tail.bace; return bace % exponent < 0 ? (bace % exponent) + exponent : bace % exponent; }
