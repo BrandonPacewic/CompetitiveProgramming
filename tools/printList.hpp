7731d6c78f786814f2890b18ce5f6e40a71e164d
Brandon <pacewicbrandon@gmail.com>
2021-12-28 21:10:00 -0800

    added start and end pointers

--- tools/printList.hpp
+++ tools/printList.hpp
@@ -1,12 +1,15 @@
 template<typename T_List>
-void printList(const T_List &List, const bool space = true, const bool new_line = true) {
-    for (int i = 0; i < List.size(); i++) {
-        cout << List[i];
+void print_list(const T_List &List, const bool space = true, const bool new_line = true, int start = -1, int end = -1) {
+if (start == -1) start = 0;
+if (end == -1) end = List.size();
+
+    for (int i = start; i < end; i++) {
+        std::cout << List[i];
         
-        if (space) cout << ' ';
+        if (space) std::cout << ' ';
     }    
         if (new_line)
-            cout << '\n';
+            std::cout << '\n';
         else 
-            cout << ' ';
+            std::cout << ' ';
 }
