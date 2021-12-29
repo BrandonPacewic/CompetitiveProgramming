d4f45d52adfa04307d36a27e4537b5553f21e82a
Brandon <pacewicbrandon@gmail.com>
2021-12-29 08:31:26 -0800

    contest save

---
+++ codeForces/contests/goodBye2021/A.cc
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+
+//dbg
+#ifdef DBG_MODE
+int64_t DBG_COUNT = 0;
+void DBG_OUT() { cerr << endl; DBG_COUNT++; }
+template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
+template<typename T_List> void testList(T_List List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__: ("; DBG_COUNT++; for (int i = 0; i < List.size(); i++) { cerr << List[i] << (i < List.size() - 1 ? ", " : ")\n"); } }
+#define testArgs(...) cerr << '#' << DBG_COUNT << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", DBG_OUT(__VA_ARGS__)
+#else
+template<typename T_List> void testList(T_List List) { return; }
+#define testArgs(...)
+#endif
+
+
+void runCase() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+
+    for (auto &a : A)
+        cin >> a;
+
+    set<int> used;
+
+    for (int i = 0; i < N; i++) {
+        if (!used.count(A[i])) {
+            used.insert(A[i]);
+        } else {
+            used.insert(-A[i]);
+        }
+    }
+
+    cout << used.size() << '\n';
+}
+
+
+int main() {
+#ifdef TEXT_IO
+    freopen("in.txt", "r", stdin);
+    freopen("ou.txt", "w", stdout);
+#endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test_cases;
+    cin >> test_cases;
+
+    while (test_cases--) {
+        runCase();
+        cout << flush;
+    }
+}
