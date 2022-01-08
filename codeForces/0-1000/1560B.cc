19198c75dc54b7c58221408334059d4b4c8a62e7
Brandon <pacewicbrandon@gmail.com>
2022-01-08 14:17:11 -0800

    rewrite of solution

--- codeForces/0-1000/1560B.cc
+++ codeForces/0-1000/1560B.cc
@@ -1,39 +1,58 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
 using namespace std;
 
 //dbg
-#define DBG_MODE
-long long DBG_COUNT = 0ll;
-void DBG_OUT() { cerr << endl; DBG_COUNT += 1ll; }
-template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
 #ifdef DBG_MODE
-template<typename T_Ints> void testList(T_Ints List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__: ("; DBG_COUNT += 1ll; for (int i = 0; i < List.size(); i++) { cout << List[i] << (i < List.size() - 1 ? ", " : ")\n"); } }
+int64_t DBG_COUNT = 0;
+void DBG_OUT() { cerr << endl; DBG_COUNT++; }
+template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
+template<typename T_List> void testList(T_List List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__: ("; DBG_COUNT++; for (int i = 0; i < List.size(); i++) { cerr << List[i] << (i < List.size() - 1 ? ", " : ")\n"); } }
 #define testArgs(...) cerr << '#' << DBG_COUNT << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", DBG_OUT(__VA_ARGS__)
 #else
-template<typename T_Ints> void testList(T_Ints List) { return; }
+template<typename T_List> void testList(T_List List) { return; }
 #define testArgs(...)
 #endif
 
 
-//https://codeforces.com/problemset/problem/1560/B
+// https://codeforces.com/problemset/problem/1560/B
 void runCase() {
     int A, B, C;
     cin >> A >> B >> C;
-    int N = abs(A - B);
+    int N = abs(A - B) * 2;
 
-    if (A > N || B > N || C > N) {
-        cout << "-1\n";
+    if (C > N || max(A, B) > N) {
+        cout << -1 << '\n';
         return;
-    } else
-        cout << (C + (N/ 2) > N ? C - (N/ 2) : C + (N/ 2)) <<  '\n';
+    }
+
+    N /= 2;
+    cout << (C > N ? C - N : C + N) << '\n';
 }
 
+
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int test_cases;
     cin >> test_cases;
 
-    for (int tc = 1; tc <= test_cases; tc++) {
+    while (test_cases--) {
         runCase();
-        cerr << flush;
+        cout << flush;
     }
 }
