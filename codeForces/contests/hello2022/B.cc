656752ff93daebf55bb1d54aecd6962ab9cdf114
Brandon <pacewicbrandon@gmail.com>
2022-01-09 13:50:24 -0800

    should word but getting some weird bug

--- codeForces/contests/hello2022/B.cc
+++ codeForces/contests/hello2022/B.cc
@@ -27,29 +27,33 @@ template<typename T_List> void testList(T_List List) { return; }
 #define testArgs(...)
 #endif
 
-#ifdef DBG_MODE
-template<typename T_Vector> void testListInList(T_Vector List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__:("; DBG_COUNT++; for (int i = 0; i < List.size(); i++) { cerr << '('; for (int k = 0; k < List[i].size(); k++) cerr << List[i][k] << (k < List[i].size() - 1 ? ", " : ")"); cerr << (i < List.size() - 1 ? ", " : ")\n"); } }
-#else
-template<typename T_Vector> void testListInList(T_Vector List) { return; }
-#endif
-
 
 void runCase() {
     int N;
     cin >> N;
-    vector<array<int, 4>> seg(N);
+    vector<array<int, 3>> segs(N);
 
-    for (auto &nseg : seg) {
-        cin >> nseg[0] >> nseg[1] >> nseg[2];
-        nseg[3] = nseg[1] - nseg[0];
+    for (auto &seg : segs)
+        cin >> seg[0] >> seg[1] >> seg[2];
+
+    for (int s = 1; s <= N; s++) {
+        int min_index = segs[0][0], max_index = segs[0][1];
+
+        for (int i = 1; i < s; i++) {
+            if (segs[i][0] <= segs[min_index][0] && segs[i][2] < segs[min_index][2])
+                min_index = i;
+
+            if (segs[i][1] >= segs[max_index][1] && segs[i][2] < segs[max_index][2])
+                max_index = i;
+        }
+
+        if (segs[min_index][0] == segs[max_index][0]) {
+            cout << segs[max_index][2];
+        } else {
+            cout << segs[min_index][2] + segs[max_index][2];
+        }
     }
 
-    testListInList(seg);
-    vector<int> totals(N);
-
-    for (int i = 0; i < N; i++) {
-
-    }
 }
 
 
