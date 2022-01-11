bca42eaa6d1cb99c125885b4ffc97e3bef4c0a06
Brandon <pacewicbrandon@gmail.com>
2022-01-11 15:36:49 -0800

    added func

---
+++ tools/printEmptyMatrix.hpp
@@ -0,0 +1,12 @@
+template<typename T>
+void print_empty_matrix(const &int _x, const &int _y, T _a, T _b, std::function<bool(int, int)> condition, const bool space = false) {
+	for (int x = 0; x < _x; x++) {
+		for (int y = 0; y < _y; y++) {
+			std::cout << (condition(x, y) ? _a : _b);
+
+			if (space) std::cout << ' ';
+		}
+
+		std::cout << '\n';
+	}
+}
