6c8ecc5e1ae8ea05e9049ecfd8ed8d2ccc99d579
Brandon <pacewicbrandon@gmail.com>
2022-01-11 19:07:17 -0800

    folder name changes

--- tools/testListInList.hpp
+++
@@ -1,5 +0,0 @@
-#ifdef DBG_MODE
-template<typename T_Vector> void testListInList(T_Vector List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__:("; DBG_COUNT++; for (int i = 0; i < List.size(); i++) { cerr << '('; for (int k = 0; k < List[i].size(); k++) cerr << List[i][k] << (k < List[i].size() - 1 ? ", " : ")"); cerr << (i < List.size() - 1 ? ", " : ")\n"); } }
-#else
-template<typename T_Vector> void testListInList(T_Vector List) { return; }
-#endif
--- tools/testSet.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_SET
-#define TEST_SET
-#ifdef DBG_MODE
-template<typename T_SET> void testSet(T_SET SET) { cerr << '#' << DBG_COUNT << " __SET_ARGS__: ( "; DBG_COUNT++; for (auto ch : SET) cerr << ch << ' '; cerr << ")\n"; }
-#else
-template<typename T_SET> void testSet(T_SET SET) { return; }
-#endif
-#endif
