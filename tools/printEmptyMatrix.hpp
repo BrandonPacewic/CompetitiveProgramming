83857bcc00762a95d67e0d845b2bc43c54b90739
Brandon <pacewicbrandon@gmail.com>
2022-01-11 18:57:45 -0800

    update to include func class

--- tools/printEmptyMatrix.hpp
+++ tools/printEmptyMatrix.hpp
@@ -1,12 +1,12 @@
-template<typename T>
-void print_empty_matrix(const &int _x, const &int _y, T _a, T _b, std::function<bool(int, int)> condition, const bool space = false) {
-	for (int x = 0; x < _x; x++) {
-		for (int y = 0; y < _y; y++) {
-			std::cout << (condition(x, y) ? _a : _b);
+template<typename T, class T_func>
+void print_empty_matrix(const int &_x, const int &_y, T _a, T _b, T_func condition, const bool space = false) {
+    for (int x = 0; x < _x; x++) {
+        for (int y = 0; y < _y; y++) {
+            std::cout << (condition(x, y) ? _b : _a);
 
-			if (space) std::cout << ' ';
-		}
+            if (space) std::cout << ' ';
+        }
 
-		std::cout << '\n';
-	}
+        std::cout << '\n';
+    }
 }
