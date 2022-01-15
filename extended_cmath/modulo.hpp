a3d0a482e5eb97a5d10c9f88021673d5b6f0fbac
Brandon <pacewicbrandon@gmail.com>
2022-01-15 12:30:24 -0800

    added type

--- extended_cmath/modulo.hpp
+++ extended_cmath/modulo.hpp
@@ -4,6 +4,6 @@ template<typename T> modulo<T, decltype(mod)> operator<(const T& front, decltype
 template<typename T_type> T_type operator>(modulo<T_type, decltype(mod)> tail, T_type exponent) { T_type& bace = tail.bace; return bace % exponent < 0 ? (bace % exponent) + exponent : bace % exponent; }
 
 template<typename T, typename T_type>
-T mod_const(T _a, T _b) {
+T mod_const(T_type _a, T_type _b) {
 	return T(_a <mod> _b);
 }
