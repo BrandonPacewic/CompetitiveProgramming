cfbdd47d601eeafdd153e994c51468a25511dc9f
Brandon <pacewicbrandon@gmail.com>
2022-01-16 20:21:56 -0800

    inital commit

---
+++ extended_cmath/long_factorials.cc
@@ -0,0 +1,56 @@
+#include <array>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+const int MAX = 500;
+
+template<typename T>
+void print_reverse(T iterable) {
+	for (int i = iterable.size() - 1; i >= 0; i--) {
+		std::cout << iterable[i];
+	}
+
+	std::cout << '\n';
+}
+
+
+std::vector<int> long_fatorials(int n) {
+	std::vector<int> res;
+	res.push_back(1);
+
+	auto multiply = [&](int itr) -> void {
+		int carry = 0;
+
+		for (int i = 0; i < res.size(); i++) {
+			int product = res[i] * itr + carry;
+			res[i] = product % 10;
+			carry = product / 10;
+		}
+
+		while (carry) {
+			res.push_back(carry % 10);
+			carry /= 10;
+		}
+	};
+
+	for (int itr = 2; itr < n + 1; itr++) {
+		multiply(itr);
+	}
+
+	assert(res.size() <= MAX);
+
+	return res;
+}
+
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int N;
+	std::cin >> N;
+
+	auto res = long_fatorials(N);
+	print_reverse(res);
+}
