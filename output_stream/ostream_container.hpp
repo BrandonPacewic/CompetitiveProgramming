b1e35524ca657109215f9d85619468b74147c396
Brandon <pacewicbrandon@gmail.com>
2022-01-23 11:32:11 -0800

    inital commit

---
+++ output_stream/ostream_container.hpp
@@ -0,0 +1,1 @@
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
