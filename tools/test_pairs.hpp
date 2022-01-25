f7170a558fd73210dcb01099318912f366cab993
Brandon <pacewicbrandon@gmail.com>
2022-01-24 17:11:14 -0800

    removed file

--- tools/test_pairs.hpp
+++
@@ -1,6 +0,0 @@
-//dbg pairs
-#ifdef DBG_MODE
-template<typename T_Pairs> void testPairs(T_Pairs Pairs) { cerr << '#' << DBG_COUNT << " __PAIR_ARGS__: ("; DBG_COUNT++; for (int i = 0; i < Pairs.size(); i++) { cerr << Pairs[i].first << '-' << Pairs[i].second << (i < Pairs.size() - 1 ? ", " : ")\n"); } } 
-#else
-template<typename T_Pairs> void testPairs(T_Pairs Pairs) { return; }
-#endif
