88a748e8ecfd49dec3105254ff3120c359eea968
Brandon <pacewicbrandon@gmail.com>
2022-02-24 17:54:22 -0800

    inital commit

---
+++ tools/no_std_dbg.hpp
@@ -0,0 +1,12 @@
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+//dbg
+#ifdef DBG_MODE
+int64_t DBG_COUNT = 0;
+void DBG_OUT() { cerr << endl; DBG_COUNT++; }
+template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
+#define test(...) cerr << '#' << DBG_COUNT << " [" << #__VA_ARGS__ << "]:", DBG_OUT(__VA_ARGS__)
+#else
+#define test(...)
+#endif
