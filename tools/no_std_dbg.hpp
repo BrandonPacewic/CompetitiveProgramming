a418e6ce3249d38869eecbdcda49a86056d4392a
Brandon <pacewicbrandon@gmail.com>
2022-02-24 18:21:09 -0800

    update to add std to endl

--- tools/no_std_dbg.hpp
+++ tools/no_std_dbg.hpp
@@ -4,7 +4,7 @@ template<typename T_container, typename T = typename std::enable_if<!std::is_sam
 //dbg
 #ifdef DBG_MODE
 int64_t DBG_COUNT = 0;
-void DBG_OUT() { std::cerr << endl; DBG_COUNT++; }
+void DBG_OUT() { std::cerr << std::endl; DBG_COUNT++; }
 template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { std::cerr << ' ' << K; DBG_OUT(T...); }
 #define test(...) std::cerr << '#' << DBG_COUNT << " [" << #__VA_ARGS__ << "]:", DBG_OUT(__VA_ARGS__)
 #else
