6c671955cb542c14c668e778d9cc19ef5fa272bb
Brandon <pacewicbrandon@gmail.com>
2022-03-07 19:07:32 -0800

    added basic uses

---
+++ mod/static_mod_type.cc
@@ -0,0 +1,111 @@
+/*
+ * Basic static mod type, prevents the value from ever going above the Ceil,
+ * used for codeforces problems where they state at the end of the problem,
+ * "Because the answer can be really really big output the answer mod x". This 
+ * has also been used in different google code jam rounds.
+ */
+
+#include <cstdint>
+#include <istream>
+#include <ostream>
+
+// TODO: Check overflow; default is int
+typedef int ceil_num;
+
+template<typename _Tp, const ceil_num Ceil>
+struct static_mod_type {
+	_Tp value;
+
+	static_mod_type() : value{0} {}
+	static_mod_type(_Tp _value) : value{_value} {}
+	~static_mod_type() = default;
+
+	explicit operator int() const { return value; }
+	explicit operator int64_t() const { return value; }
+	explicit operator float() const { return value; }
+	explicit operator double() const { return value; }
+	explicit operator long double() const { return value; }
+
+	static_mod_type& operator%=(const static_mod_type& other) {
+		value %= other.value;
+		if (value < 0) { value += other.value; }
+		return *this;
+	}
+
+	static_mod_type& operator+=(const static_mod_type& other) {
+		value += other.value;
+		return *this %= Ceil;
+	}
+
+	static_mod_type& operator-=(const static_mod_type& other) {
+		value -= other.value;
+		return *this %= Ceil;
+	}
+
+	static_mod_type& operator*=(const static_mod_type& other) {
+		value *= other.value;
+		return *this %= Ceil;
+	}
+
+	static_mod_type& operator/=(const static_mod_type& other) 
+	{ return value /= other.value; }
+
+	static_mod_type operator+(const static_mod_type& other) const {
+		auto tmp = *this;
+		return tmp += other;
+	}
+
+	static_mod_type operator-(const static_mod_type& other) const {
+		auto tmp = *this;
+		return tmp -= other;
+	}
+
+	static_mod_type operator*(const static_mod_type& other) const {
+		auto tmp = *this;
+		return tmp *= other;
+	}
+
+	static_mod_type operator/(const static_mod_type& other) const {
+		auto tmp = *this;
+		return tmp /= other;
+	}
+
+	static_mod_type operator%(const static_mod_type& other) const {
+		auto tmp = *this;
+		return tmp %= other;
+	}
+
+	static_mod_type operator++() {
+		++value;
+		return *this %= Ceil;
+	}
+
+	static_mod_type operator--() {
+		--value;
+		return *this %= Ceil;
+	}
+
+	bool operator==(const static_mod_type& other) const 
+	{ return value == other.value; }
+
+	bool operator!=(const static_mod_type& other) const
+	{ return value != other.value; }
+
+	bool operator<(const static_mod_type& other) const
+	{ return value < other.value; }
+
+	bool operator>(const static_mod_type& other) const
+	{ return value > other.value; }
+
+	bool operator<=(const static_mod_type& other) const
+	{ return value <= other.value; }
+
+	bool operator>=(const static_mod_type& other) const
+	{ return value >= other.value; }
+
+	friend std::ostream& operator<<(std::ostream& os, const static_mod_type& a)
+	{ return os << a.value; }
+
+	friend std::istream& operator>>(std::istream& is, static_mod_type& a)
+	{ return is >> a.value; }
+};
