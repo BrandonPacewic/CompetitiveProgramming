9baa649620a1b5036c69a38ac9223f4659e5cd92
Brandon <pacewicbrandon@gmail.com>
2022-03-14 14:16:44 -0700

    need more testing

--- matrix/basic_matrix.cc
+++ matrix/basic_matrix.cc
@@ -8,12 +8,11 @@
 
 // This basic matrix is almost identical to uniform_matrix, just renamed so 
 // I can search for the version of this type that contains all the definitions
-// in one file so I can quickly fnid it while in contest
+// in one file while in contest
 
 #include <cstddef>
 #include <iostream>
 #include <stdexcept>
-#include <vector>
 
 template<typename _Tp>
 struct _basic_matrix_row {
@@ -34,7 +33,6 @@ struct _basic_matrix_row {
 
     int assign_back(const _Tp& new_element) try {
         elements[current_cell_index++] = new_element;
-
         return current_cell_index;
     }
     catch (const std::out_of_range& error) {
@@ -52,19 +50,21 @@ struct basic_matrix {
     basic_matrix(std::size_t _size) 
         : rows{new _basic_matrix_row<_Tp>[_size]}, sz{_size},
         current_row_index{0} { 
-        for (int i = 0; i < int(sz); i++) {
+        for (int i = 0; i < int(sz); ++i) {
             rows[i] = _basic_matrix_row<_Tp>(sz);
         }
     }
 
-    std::size_t size() const noexcept { return sz; }
+    std::size_t size() const { return sz; }
 
     _basic_matrix_row<_Tp>& operator[](int index) const { return rows[index]; }
 
     void assign_back(const _Tp& new_element) try {
         int current = rows[current_row_index].assign_back(new_element);
 
-        if (current == sz) { ++current_row_index; }
+        if (current == sz) { 
+            ++current_row_index; 
+        }
     }
     catch (const std::out_of_range& error) {
         std::cerr << "assign_back has no rows to assign" << '\n';
@@ -73,7 +73,7 @@ struct basic_matrix {
 
     // TODO: lambda function should have type of void
     template<class _Fun>
-    void for_all(_Fun function) const noexcept {
+    void for_all(_Fun function) const {
         for (int _row = 0; _row < int(sz); ++_row) {
             for (int _cell = 0; _cell < int(sz); ++_cell) {
                 function(rows[_row][_cell], _row, _cell);
