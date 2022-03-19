5d0d962c1fc4b033b37e3cb022faccb6f53eeaf3
Brandon <pacewicbrandon@gmail.com>
2022-03-19 10:32:44 -0700

    changed pre deincrement to post deincrement

--- templates/code_forces.cc
+++ templates/code_forces.cc
@@ -59,7 +59,7 @@ int main() {
     int test_cases;
     std::cin >> test_cases;
 
-    while (--test_cases) {
+    while (test_cases--) {
         run_case();
         std::cout << std::flush;
     }
