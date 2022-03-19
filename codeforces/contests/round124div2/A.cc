b3d262b06fe99ef9f30a3989bdfb049ca5c9a69a
Brandon <pacewicbrandon@gmail.com>
2022-03-19 10:32:02 -0700

    cool

---
+++ codeforces/contests/round124div2/A.cc
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <vector>
+
+// dbg output stream handling for pairs
+template<typename A, typename B> 
+std::ostream& operator<<(std::ostream& os, const std::pair<A, B>& p) { 
+    return os << '(' << p.first << ", " << p.second << ')'; 
+}
+
+// dbg output stream handling for containers excluding type std::string
+template<typename T_container, typename T = typename std::enable_if<
+        !std::is_same<T_container, std::string>::value, 
+        typename T_container::value_type>::type> 
+std::ostream& operator<<(std::ostream& os, const T_container& A) { 
+    std::string sep; 
+    os << '{';
+
+    for (const T& a : A) { 
+        os << sep << a, sep = ", ";
+    }
+
+    return os << '}'; 
+}
+
+//dbg
+#ifdef DBG_MODE
+void dbg_out() { std::cerr << std::endl; }
+template<typename Head, typename... Tail> 
+void dbg_out(Head A, Tail... B) { std::cerr << ' ' << A; dbg_out(B...); }
+#define test(...) std::cerr << "[" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define test(...)
+#endif
+
+
+void run_case() {
+    using namespace std;
+    int N;
+    cin >> N;
+    int athletes = pow(2, N);
+
+    cout << athletes - 1 << '\n';
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int test_cases;
+    std::cin >> test_cases;
+
+    while (test_cases--) {
+        run_case();
+        std::cout << std::flush;
+    }
+
+    return 0;
+}
