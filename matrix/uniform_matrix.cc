7a097b8161f2beb4d885914fbc88f5cf7f9d9644
Brandon <pacewicbrandon@gmail.com>
2022-03-19 11:14:08 -0700

    changes to basic matrix

--- matrix/uniform_matrix.cc
+++ matrix/uniform_matrix.cc
@@ -3,15 +3,12 @@
  *
  * Developed and tested by Brandon Pacewic
  * 
- * basic_matrix.cc
+ * uniform_matrix.cc
  */
-
-// This basic matrix is almost identical to uniform_matrix, just renamed so 
-// I can search for the version of this type that contains all the definitions
-// in one file while in contest
 
 #include <cstddef>
 #include <iostream>
+#include <functional>
 #include <stdexcept>
 
 template<typename _Tp>
@@ -20,10 +17,10 @@ struct _basic_matrix_row {
     std::size_t sz;
     int current_cell_index;
 
-    _basic_matrix_row() { }
+    _basic_matrix_row() {}
 
     _basic_matrix_row(std::size_t _size) 
-        : elements{new _Tp[_size]}, sz{_size}, current_cell_index{0} { }
+        : elements{new _Tp[_size]}, sz{_size}, current_cell_index{0} {}
 
     ~_basic_matrix_row() { delete[] elements; }
 
@@ -71,14 +68,27 @@ struct basic_matrix {
         throw error;
     }
 
-    // TODO: lambda function should have type of void
-    template<class _Fun>
-    void for_all(_Fun function) const {
+    void for_all(std::function<void(_Tp, int, int)> lambda) const {
         for (int _row = 0; _row < int(sz); ++_row) {
             for (int _cell = 0; _cell < int(sz); ++_cell) {
-                function(rows[_row][_cell], _row, _cell);
+                lamdba(rows[_row][_cell], _row, _cell);
             }
         }
+    }
+
+    const void output(std::ostream& os = std::cout, 
+        const bool& space = false) const {
+        for (int row = 0; row < int(sz); ++row) {
+            for (int cell = 0; cell < int(sz); ++cell) {
+                os << rows[row][cell];
+
+                if (space && cell < int(sz) - 1) {
+                    os << ' ';
+                }
+            }
+        }
+
+        os << '\n';
     }
 };
 
