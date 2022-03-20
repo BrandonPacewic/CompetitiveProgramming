bc1b5d20d353cf5ab23c9488bdafcf13e3e0aa57
Brandon <pacewicbrandon@gmail.com>
2022-03-20 09:52:55 -0700

    hard big int problem

---
+++ google_kick_start/2022/roundA/B.cc
@@ -0,0 +1,82 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <vector>
+using namespace std;
+
+// dbg output stream handling for pairs
+template<typename A, typename B> 
+std::ostream& operator<<(std::ostream& os, const std::pair<A, B>& p) { 
+    return os << '(' << p.first << ", " << p.second << ')'; 
+}
+
+// dbg output stream handling for containers excluding type std::string
+template<typename T_container, typename T = typename std::enable_if<
+        !std::is_same<T_container, std::string>::value, 
+        typename T_container::value_type>::type> 
+std::ostream& operator<<(std::ostream& os, const T_container& A) { 
+    std::string sep; 
+    os << '{';
+
+    for (const T& a : A) { 
+        os << sep << a, sep = ", ";
+    }
+
+    return os << '}'; 
+}
+
+//dbg
+#ifdef DBG_MODE
+void dbg_out() { std::cerr << std::endl; }
+template<typename Head, typename... Tail> 
+void dbg_out(Head A, Tail... B) { std::cerr << ' ' << A; dbg_out(B...); }
+#define test(...) std::cerr << "[" << #__VA_ARGS__ << "]:", dbg_out(__VA_ARGS__)
+#else
+#define test(...)
+#endif
+
+
+void run_case(const int& tc) {
+    string N;
+    cin >> N;
+
+    int r = 0;
+
+    for (auto& ch : N) {
+        r = (r + ch - '0') % 9;
+    }
+
+    int t = (9 - r) % 9, p = 0;
+
+    while (!t && !p || p < int(N.length()) && N[p] - '0' <= t) {
+        ++p;
+    }
+
+    cout << "Case #" << tc << ": ";
+    cout << N.substr(0, p) << t << N.substr(p) << '\n';
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int test_cases;
+    std::cin >> test_cases;
+
+    for (int tc = 1; tc <= test_cases; tc++) {
+        run_case(tc);
+        std::cout << std::flush;
+    }
+
+    return 0;
+}
