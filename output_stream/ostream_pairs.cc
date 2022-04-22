7d2c32f4ac56fc2305900f5e609cd6219eb9605c
Brandon <pacewicbrandon@gmail.com>
2022-04-22 15:13:43 -0700

    New folder structure (#4)

--- output_stream/ostream_pairs.cc
+++
@@ -1,6 +0,0 @@
-#include <ostream>
-
-template<typename A, typename B> 
-std::ostream& operator<<(std::ostream& os, const std::pair<A, B>& p) { 
-    return os << '(' << p.first << ", " << p.second << ')'; 
-}
--- sorting/reverse_sort.hpp
+++
@@ -1,7 +0,0 @@
-template<typename T_Vector>
-void revSort(T_Vector &list) {
-    for (int i = 0; i < list.size() - 1; i++) {
-        int index = int(min_element(list.begin() + i, list.end()) - list.begin());
-        reverse(list.begin() + i, list.begin() + index + 1);
-    }
-}
