1818168bb3bbddb59cdb92c26c922f7f3770b431
Brandon <pacewicbrandon@gmail.com>
2022-04-22 14:37:56 -0700

    Small update to output container (#8)

--- output_stream/output_container.cc
+++ output_stream/output_container.cc
@@ -1,13 +1,13 @@
 template<typename T_container>
-void output_container(const T_container& container, const bool& space = true,
-    const bool& new_line = true) {
-    int start = 0;
-    int end = int(container.size());
-
-    for (int i = start; i < end; ++i) {
+void output_container(const T_container& container, 
+        const bool space = true, 
+        const bool new_line = true) {
+    for (std::size_t i = 0; i < container.size(); ++i) {
         std::cout << container[i];
 
-        if (space && i < end - 1) std::cout << ' ';
+        if (space && i < end - 1) {
+            std::cout << ' ';
+        }
     }
 
     std::cout << (new_line ? '\n' : ' ');
