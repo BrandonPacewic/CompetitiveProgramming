1508112e5e7bc29cd8687e87ef15ca9f38e3cd9d
Brandon <pacewicbrandon@gmail.com>
2022-04-24 21:50:10 -0700

    Update to basic for each func (#21)

--- src/container/for_each.cc
+++ src/container/for_each.cc
@@ -5,8 +5,8 @@ template<
     typename T_container, 
     typename T = typename T_container::value_type, 
     class Fun>
-void for_each(T_container& container, Fun func) {
+void for_each(T_container& container, Fun lambda) {
     for (T& item : container) {
-        func(item);
+        lambda(item);
     }
 }
