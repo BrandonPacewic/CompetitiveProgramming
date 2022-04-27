c2cc8b8c1e58cac986ee7c066310708e828da295
Brandon <pacewicbrandon@gmail.com>
2022-04-27 09:00:50 -0700

    Added int24 (#30)

---
+++ src/extended_cmath/int24.cc
@@ -0,0 +1,32 @@
+struct int24 {
+    unsigned char data[3];
+
+    int24(unsigned long val) {
+        data[0] = ( val          & 0xff);
+        data[1] = ((val >> 8)    & 0xff);
+        data[2] = ((val >> 16)   & 0xff);
+    }
+
+    void operator=(unsigned long val) {
+        data[0] = ( val          & 0xff);
+        data[1] = ((val >> 8)    & 0xff);
+        data[2] = ((val >> 16)   & 0xff);
+    }
+
+    void operator=(int24 val) {
+        data[0] = val.data[0];
+        data[1] = val.data[1];
+        data[2] = val.data[2];
+    }
+
+    unsigned long decode() const {
+        unsigned long val;
+        val = this->data[2];
+        val <<= 8;
+        val |= this->data[1];
+        val <<= 8;
+        val |= this->data[0];
+
+        return val;
+    }
+};
