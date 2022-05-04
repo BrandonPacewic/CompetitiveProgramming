42caab86b4ef7fd907f1a8dcbc5168bf733d23b8
Brandon <pacewicbrandon@gmail.com>
2022-05-04 09:06:15 -0700

    Shorter test program (#11)

--- tests/container/encoding.test.cc
+++ tests/container/encoding.test.cc
@@ -1,5 +1,6 @@
 #include "../../src/container/encoding.cc"
 
+#include <algorithm>
 #include <array>
 #include <iostream>
 #include <string>
@@ -9,152 +10,43 @@ constexpr uint16_t total_tests = 5;
 
 using encoding_output = std::vector<std::pair<char, int>>;
 
-std::array<std::pair<std::string, encoding_output>, total_tests> 
-generate_run_length_encoding_tests() {
-    std::array<std::pair<std::string, encoding_output>, total_tests> tests;
+std::array<std::pair<std::string, std::vector<std::pair<char, int>>>, total_tests> test_cases;
 
-    tests[0] = {
-        "abcdefghi",
-        {
-            {'a', 1},
-            {'b', 1},
-            {'c', 1},
-            {'d', 1},
-            {'e', 1},
-            {'f', 1},
-            {'g', 1},
-            {'h', 1},
-            {'i', 1},
-        }
-    };
+void init_test_cases() {
+    test_cases[0].first = "";
+    test_cases[0].second = {};
 
-    tests[1] = {
-        "aabcdefgh",
-        {
-            {'a', 2},
-            {'b', 1},
-            {'c', 1},
-            {'d', 1},
-            {'e', 1},
-            {'f', 1},
-            {'g', 1},
-            {'h', 1},
-        }
-    };
+    test_cases[1].first = "a";
+    test_cases[1].second = { { 'a', 1 } };
 
-    tests[2] = {
-        "arandomword",
-        {
-            {'a', 1},
-            {'r', 1},
-            {'a', 1},
-            {'n', 1},
-            {'d', 1},
-            {'o', 1},
-            {'m', 1},
-            {'w', 1},
-            {'o', 1},
-            {'r', 1},
-            {'d', 1},
-        }
-    };
+    test_cases[2].first = "aa";
+    test_cases[2].second = { { 'a', 2 } };
 
-    tests[3] = {
-        "aabbccddeeffgghhiijjkkllmmnnooppqqrrssttuuvvwwxxyyzz",
-        {
-            {'a', 2},
-            {'b', 2},
-            {'c', 2},
-            {'d', 2},
-            {'e', 2},
-            {'f', 2},
-            {'g', 2},
-            {'h', 2},
-            {'i', 2},
-            {'j', 2},
-            {'k', 2},
-            {'l', 2},
-            {'m', 2},
-            {'n', 2},
-            {'o', 2},
-            {'p', 2},
-            {'q', 2},
-            {'r', 2},
-            {'s', 2},
-            {'t', 2},
-            {'u', 2},
-            {'v', 2},
-            {'w', 2},
-            {'x', 2},
-            {'y', 2},
-            {'z', 2},
-        }
-    };
+    test_cases[3].first = "aba";
+    test_cases[3].second = { { 'a', 1 }, { 'b', 1 }, { 'a', 1 } };
 
-    tests[4] = {
-        "aaabbbcccdddeeefffggghhiiiijjkkllllmmmmnnnnooopppqqq"
-        "rrrrsssssttttuuuuvvvvwwwwxxxxyyyyzzzz",
-        {
-            {'a', 3},
-            {'b', 3},
-            {'c', 3},
-            {'d', 3},
-            {'e', 3},
-            {'f', 3},
-            {'g', 3},
-            {'h', 2},
-            {'i', 4},
-            {'j', 2},
-            {'k', 2},
-            {'l', 4},
-            {'m', 4},
-            {'n', 4},
-            {'o', 3},
-            {'p', 3},
-            {'q', 3},
-            {'r', 4},
-            {'s', 5},
-            {'t', 4},
-            {'u', 4},
-            {'v', 4},
-            {'w', 4},
-            {'x', 4},
-            {'y', 4},
-            {'z', 4},
-        }
-    };
-
-    return tests;
-}
-
-bool run_encoding_tests() {
-    auto tests = generate_run_length_encoding_tests();
-    bool passing = true;
-
-    for (uint16_t i = 0; i < total_tests; ++i) {
-        const auto& test = tests[i];
-        const auto& input = test.first;
-        const auto& expected = test.second;
-        const auto output = run_length_encoding<char>(input);
-
-        if (output != expected) {
-            std::cerr << "Test " << i + 1 << " failed.\n";
-            passing = false;
-        }
-    }
-
-    return passing;
+    test_cases[4].first = "ababa";
+    test_cases[4].second = { { 'a', 1 }, { 'b', 1 }, { 'a', 1 }, { 'b', 1 }, { 'a', 1 } };
 }
 
 int main() {
-    const bool all_tests_passed = run_encoding_tests();
+    init_test_cases();
+    bool all_passed = true;
 
-    if (all_tests_passed) {
-        std::cerr << "All encoding tests passed!" << std::endl;
-    }
-    else {
-        std::cerr << "Some encoding tests failed!" << std::endl;
-    }
+    std::cout << "Running run_length_encoding tests..." << std::endl;
 
-    return !all_tests_passed;
+    std::for_each(test_cases.begin(), test_cases.end(), [&](const auto& test_case) {
+        bool current_test_passed = run_length_encoding<char>(test_case.first) == test_case.second;
+
+        if (!current_test_passed) {
+            all_passed = false;
+            std::cout << "Failed test case: " << test_case.first << std::endl;
+        } else {
+            std::cout << "Passed test case: " << test_case.first << std::endl;
+        }
+    });
+
+    std::cout << "Finished run_length_encoding tests." << std::endl;
+
+    return !all_passed;
 }
