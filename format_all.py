21e085c483898eff8abdb9048d5e0500ab3fbdfa
Brandon <pacewicbrandon@gmail.com>
2022-05-08 17:22:01 -0700

    Temp change for hpp files (#57)

--- format_all.py
+++ format_all.py
@@ -3,7 +3,7 @@ import os
 def main():
     for dirpath, _, filenames in os.walk('./'):
         for filename in filenames:
-            if filename.endswith('.cc'):
+            if filename.endswith('.cc') or filename.endswith('.hpp'):
                 print(f'Found file {dirpath}/{filename}')
                 os.system(f'clang-format -i -style=file {dirpath}/{filename}')
 
