d920840c53a416b4af4d2a44c0893a3a68020851
Brandon <pacewicbrandon@gmail.com>
2022-05-08 17:32:40 -0700

    Reverting change now will no longer format `hpp` files (#57)

--- format_all.py
+++ format_all.py
@@ -3,7 +3,7 @@ import os
 def main():
     for dirpath, _, filenames in os.walk('./'):
         for filename in filenames:
-            if filename.endswith('.cc') or filename.endswith('.hpp'):
+            if filename.endswith('.cc'):
                 print(f'Found file {dirpath}/{filename}')
                 os.system(f'clang-format -i -style=file {dirpath}/{filename}')
 
