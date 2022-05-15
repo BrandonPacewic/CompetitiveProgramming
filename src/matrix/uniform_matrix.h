3250d149d0afba3de8ed5a8cba6beeb5108b9dac
Brandon <pacewicbrandon@gmail.com>
2022-05-15 13:53:10 -0700

    Lots of function update + even more abstraction (#61)

--- src/matrix/uniform_matrix.h
+++ src/matrix/uniform_matrix.h
@@ -155,9 +155,9 @@ class uniform_matrix {
     typedef Ty* type_pointer;
     typedef const Ty* const_type_pointer;
     typedef row_type& row_reference;
-    typedef const row_type& const_row_roference;
+    typedef const row_type& const_row_reference;
     typedef Ty& type_reference;
-    typedef const Ty& type_const_reference;
+    typedef const Ty& const_type_reference;
     typedef std::size_t size_type;
     typedef Ty value_type;
 
@@ -185,8 +185,99 @@ class uniform_matrix {
     }
 
     /**
+     *  @brief Constructs an uniform_matrix object from a linear
+     *         initializer list.
+     *  @param _list Initializer list to construct the matrix from.
+     */
+    uniform_matrix(std::initializer_list<value_type> _list)
+        : rows{std::make_unique<row_type[]>(_list.size())},
+          row_length{_list.size()} {
+        size_type i = 0;
+
+        for (row_reference row : rows) {
+            for (type_reference cell : row) {
+                cell = *(_list.begin() + i), ++i;
+
+                if (i >= _list.size()) {
+                    break;
+                }
+            }
+        }
+    }
+
+   private:
+    typedef std::initializer_list<std::initializer_list<value_type>>
+        _matrix_initializer_list;
+
+   protected:
+    inline const bool is_square(const _matrix_initializer_list& _list) const {
+        for (const std::initializer_list<value_type>& row : _list) {
+            if (row.size() != _list.size()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+   public:
+    /**
+     *  @brief Constructs an uniform_matrix object from a two dimentional
+     *         initializer list.
+     *  @param _list Initializer list to construct the matrix from.
+     *  @throw std::invalid_argument if the initializer list is not a perfect
+     *        square.
+     */
+    uniform_matrix(_matrix_initializer_list _list)
+        : rows{std::make_unique<row_type[]>(_list.size())},
+          row_length{_list.size()} {
+        if (!is_square(_list)) {
+            throw std::invalid_argument(
+                "Invalid uniform_matrix to be constructed from initializer "
+                "list. Initializer list is not a perfect square.");
+        }
+
+        for (size_type row = 0; row < row_length; ++row) {
+            for (size_type cell = 0; cell < row_length; ++cell) {
+                rows[row][cell] = _list[row][cell];
+            }
+        }
+    }
+
+    /**
+     *  @brief Constructs an uniform_matrix object from a range of values.
+     *  @param _n Number of rows and columns in the matrix.
+     *  @param _begin Iterator to the first element in the range.
+     *  @param _end Iterator to the element one past the last element in the
+     *         range.
+     *  @throw std::invalid_argument if the range is not a perfect square.
+     */
+    template <class InputIterator>
+    uniform_matrix(size_type _n, InputIterator _begin, InputIterator _end)
+        : rows{std::make_unique<row_type[]>(_n)}, row_length{_n} {
+        for (row_reference row : rows) {
+            for (type_reference cell : row) {
+                cell = *_begin, ++_begin;
+
+                if (_begin == _end) {
+                    break;
+                }
+            }
+        }
+
+        if (_begin != _end) {
+            throw std::invalid_argument(
+                "Invalid uniform_matrix to be constructed from range. "
+                "Range is not a perfect square.");
+        }
+    }
+
+    /**
      *   @brief Returns the number of rows in the matrix.
      *   @return Number of rows in the matrix.
+     *
+     *  In @a uniform_matrix the number of rows will always be the same
+     *  as the number of columns.
      */
     const size_type size() const { return row_length; }
 
@@ -237,7 +328,7 @@ class uniform_matrix {
      *  @brief Returns a constant reference to the first element in the matrix.
      *  @return constant reference to the first element in the matrix.
      */
-    const_row_roference front() const noexcept { return *begin(); }
+    const_row_reference front() const noexcept { return *begin(); }
 
     /**
      *  @brief Returns a reference to the last element in the matrix.
@@ -249,7 +340,7 @@ class uniform_matrix {
      *  @brief Returns a constant reference to the last element in the matrix.
      *  @return constant reference to the last element in the matrix.
      */
-    const_row_roference back() const noexcept { return *(end() - 1); }
+    const_row_reference back() const noexcept { return *(end() - 1); }
 
     /**
      *  @brief Returns a reference to the element at the given index.
@@ -257,7 +348,13 @@ class uniform_matrix {
      *  @return reference to the element at the given index.
      */
     row_reference operator[](const size_type& index) { return rows[index]; }
-    const_row_roference operator[](const size_type& index) const {
+
+    /**
+     *  @brief Returns a constant reference to the element at the given index.
+     *  @param index Index of the element to return.
+     *  @return constant reference to the element at the given index.
+     */
+    const_row_reference operator[](const size_type& index) const {
         return rows[index];
     }
 
@@ -270,9 +367,9 @@ class uniform_matrix {
      */
     template <class UnaryPredicate>
     bool any_of(UnaryPredicate predicate) const {
-        for (row_pointer it = begin(); it != end(); ++it) {
-            for (type_pointer jt = (*it).begin(); jt != (*it).end(); ++jt) {
-                if (predicate(*jt)) {
+        for (const_row_reference& row : *this) {
+            for (const_type_reference& cell : row) {
+                if (predicate(cell)) {
                     return true;
                 }
             }
@@ -290,9 +387,9 @@ class uniform_matrix {
      */
     template <class UnaryPredicate>
     bool all_of(UnaryPredicate predicate) const {
-        for (row_pointer it = begin(); it != end(); ++it) {
-            for (type_pointer jt = (*it).begin(); jt != (*it).end(); ++jt) {
-                if (!predicate(*jt)) {
+        for (const_row_reference& row : *this) {
+            for (const_type_reference& cell : row) {
+                if (!predicate(cell)) {
                     return false;
                 }
             }
@@ -332,10 +429,9 @@ class uniform_matrix {
     ReturnType count(const value_type& value) const {
         ReturnType count = 0;
 
-        for (const_row_pointer it = begin(); it != end(); ++it) {
-            for (const_type_pointer jt = (*it).begin(); jt != (*it).end();
-                 ++jt) {
-                if (*jt == value) {
+        for (const_row_reference& row : *this) {
+            for (const_type_reference& cell : row) {
+                if (cell == value) {
                     ++count;
                 }
             }
@@ -355,10 +451,9 @@ class uniform_matrix {
     ReturnType count(UnaryPredicate predicate) const {
         ReturnType count = 0;
 
-        for (const_row_pointer it = begin(); it != end(); ++it) {
-            for (const_type_pointer jt = (*it).begin(); jt != (*it).end();
-                 ++jt) {
-                if (predicate(*jt)) {
+        for (const_row_reference& row : *this) {
+            for (const_type_reference& cell : row) {
+                if (predicate(cell)) {
                     ++count;
                 }
             }
@@ -372,9 +467,9 @@ class uniform_matrix {
      *  @param value The value to fill the matrix with.
      */
     void fill(const value_type& value) {
-        for (row_pointer it = begin(); it != end(); ++it) {
-            for (type_pointer jt = it->begin(); jt != it->end(); ++jt) {
-                *jt = value;
+        for (row_reference& row : *this) {
+            for (type_reference& cell : row) {
+                cell = value;
             }
         }
     }
@@ -387,12 +482,35 @@ class uniform_matrix {
      */
     template <class UnaryPredicate>
     void fill(const value_type& value, UnaryPredicate predicate) {
-        for (row_pointer it = begin(); it != end(); ++it) {
-            for (type_pointer jt = it->begin(); jt != it->end(); ++jt) {
-                if (predicate(*jt)) {
-                    *jt = value;
+        for (row_reference& row : *this) {
+            for (type_reference& cell : row) {
+                if (predicate(cell)) {
+                    cell = value;
                 }
             }
+        }
+    }
+
+    /**
+     *  @brief Fills the whole matrix with a range of values.
+     *  @tparam InputIterator The type of the input iterator.
+     *  @param first The first element in the range.
+     *  @param last The last element in the range.
+     *  @throw std::invalid_argument if the range is not the same size as the
+     *         matrix.
+     */
+    template <class InputIterator>
+    void fill(InputIterator first, InputIterator last) {
+        for (row_reference& row : *this) {
+            for (type_reference& cell : row) {
+                cell = *(first), ++first;
+            }
+        }
+
+        if (first != last) {
+            throw std::invalid_argument(
+                "InputIterator range is not of size "
+                "uniform_matrix size.");
         }
     }
 
@@ -465,9 +583,9 @@ class uniform_matrix {
      *  size_type reference of the current column.
      */
     void for_each(std::function<void(value_type&)> function) {
-        for (size_type row = 0; row < row_length; ++row) {
-            for (size_type cell = 0; cell < row_length; ++cell) {
-                function(rows[row][cell]);
+        for (row_reference& row : *this) {
+            for (type_reference& cell : row) {
+                function(cell);
             }
         }
     }
@@ -500,9 +618,9 @@ class uniform_matrix {
      *  uniform_matrix::for_each function insted.
      */
     void iota(value_type start) {
-        for (row_pointer it = begin(); it != end(); ++it) {
-            for (type_pointer jt = (*it).begin(); jt != (*it).end(); ++jt) {
-                *jt = start, ++start;
+        for (row_reference& row : *this) {
+            for (type_reference& cell : row) {
+                cell = start, ++start;
             }
         }
     }
@@ -523,25 +641,44 @@ class uniform_matrix {
         typedef _Ty value_type;
         typedef std::size_t size_type;
 
+        /**
+         *  @brief Constructor.
+         *  @param _matrix The matrix to convert.
+         */
         __list_matrix_elements(const uniform_matrix& _matrix)
             : _linear_size{_matrix.total_size()},
               _linear_data{std::make_unique<value_type[]>(_linear_size)} {
             size_type linear_index = 0;
 
-            for (const_row_pointer it = _matrix.begin(); it != _matrix.end();
-                 ++it) {
-                for (const_type_pointer jt = it->begin(); jt != it->end();
-                     ++jt) {
-                    _linear_data[linear_index] = *jt, ++linear_index;
+            for (const_row_reference row : _matrix) {
+                for (const_reference cell : row) {
+                    _linear_data[linear_index] = cell, ++linear_index;
                 }
             }
         }
 
+        /**
+         *  @brief Returns a constant pointer to the first element in the
+         *         container.
+         *  @return constant pointer to the first element in the container.
+         */
         const const_pointer begin() const { return _linear_data.get(); }
+
+        /**
+         *  @brief Returns a constant pointer to the element one past the end
+         *         of the container.
+         *  @return constant pointer to the element one past the end of the
+         *          container.
+         */
         const const_pointer end() const {
             return _linear_data.get() + _linear_size;
         }
 
+        /**
+         *  @brief Returns a reference to the element at the given index.
+         *  @param index The index of the element to return.
+         *  @return reference to the element at the given index.
+         */
         const_reference operator[](const size_type& index) const {
             return _linear_data[index];
         }
@@ -557,24 +694,11 @@ class uniform_matrix {
      *  @brief Tests if the matrix is sorted or not.
      *  @return true if the matrix is sorted, false otherwise.
      */
-    bool is_sorted() const {
-        _list_matrix_elements linear_matrix{*this};
-        return std::is_sorted(linear_matrix.begin(), linear_matrix.end());
-    }
-
-    template <class BinaryPredicate>
+    template <class BinaryPredicate = std::less<value_type>>
     bool is_sorted(BinaryPredicate predicate) const {
-        for (row_pointer it = begin(); it != end(); ++it) {
-            for (type_pointer jt = (*it).begin(); jt != (*it).end(); ++jt) {
-                if (jt != (*it).begin()) {
-                    if (predicate(*jt, *(jt - 1))) {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        return true;
+        _list_matrix_elements linear_matrix{*this};
+        return std::is_sorted(linear_matrix.begin(), linear_matrix.end(),
+                              predicate);
     }
 
     const void output(const bool& space = true) const {
@@ -605,20 +729,10 @@ class uniform_matrix {
         }
     }
 
-    bool rows_sorted() const {
-        for (const_row_pointer it = begin(); it != end(); ++it) {
-            if (!std::is_sorted(it->begin(), it->end())) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    template <class BinaryPredicate>
+    template <class BinaryPredicate = std::less<value_type>>
     bool rows_sorted(BinaryPredicate predicate) const {
-        for (const_row_pointer it = begin(); it != end(); ++it) {
-            if (!std::is_sorted(it->begin(), it->end(), predicate)) {
+        for (const_row_reference& row : *this) {
+            if (!std::is_sorted(row.begin(), row.end(), predicate)) {
                 return false;
             }
         }
@@ -626,52 +740,17 @@ class uniform_matrix {
         return true;
     }
 
-    void sort() {
-        size_type item_count = 0;
-        std::unique_ptr<value_type[]> listed_elements{
-            std::make_unique<value_type[]>(this->total_size())};
-
-        for (row_pointer it = begin(); it != end(); ++it) {
-            for (type_pointer jt = (*it).begin(); jt != (*it).end(); ++jt) {
-                listed_elements[item_count] = *jt, ++item_count;
-            }
-        }
-
-        std::sort(listed_elements.get(),
-                  listed_elements.get() + this->total_size());
-        item_count = 0;
-
-        for (row_pointer it = begin(); it != end(); ++it) {
-            for (type_pointer jt = (*it).begin(); jt != (*it).end(); ++jt) {
-                *jt = listed_elements[item_count], ++item_count;
-            }
-        }
-    }
-
-    template <class BinaryPredicate>
+    template <class BinaryPredicate = std::less<value_type>>
     void sort(BinaryPredicate predicate) {
         _list_matrix_elements linear_matrix{*this};
         std::sort(linear_matrix.begin(), linear_matrix.end(), predicate);
-        size_type item_count = 0;
-
-        // TODO: Need to add a new fill function that takes a range of elements.
-        for (row_pointer it = begin(); it != end(); ++it) {
-            for (type_pointer jt = (*it).begin(); jt != (*it).end(); ++jt) {
-                *jt = linear_matrix[item_count], ++item_count;
-            }
-        }
+        this->fill(linear_matrix.begin(), linear_matrix.end());
     }
 
-    void sort_rows() {
-        for (row_pointer it = begin(); it != end(); ++it) {
-            std::sort((*it).begin(), (*it).end());
-        }
-    }
-
-    template <class BinaryPredicate>
+    template <class BinaryPredicate = std::less<value_type>>
     void sort_rows(BinaryPredicate predicate) {
-        for (row_pointer it = begin(); it != end(); ++it) {
-            std::sort((*it).begin(), (*it).end(), predicate);
+        for (row_reference& row : *this) {
+            std::sort(row.begin(), row.end(), predicate);
         }
     }
 
