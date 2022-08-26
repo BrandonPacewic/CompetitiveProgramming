756b497653502939558a688fa045ea8ef5c4a196
Brandon <pacewicbrandon@gmail.com>
2022-08-25 21:47:53 -0700

    Start of library

---
+++ cpl/inc/core.h
@@ -0,0 +1,28 @@
+// core.h internal header
+
+// Copyright (c) Brandon Pacewic
+// SPDX-License-Identifier: MIT
+
+#pragma once
+#ifndef CORE_H_
+#define CORE_H_
+
+#define CPL_STRINGIZE_(S) #S
+#define CPL_STRINGIZE(S) CPL_STRINGIZE_(S)
+
+// CPL compiler checks
+#ifndef __cplusplus
+#pragma message(__FILE__ "(" CPL_STRINGIZE(__LINE__) "): CPL: Unexpected compiler, expected C++ compiler.")
+#error Error in C++ CPL usage; Unsupported compiler.
+#endif  // __cplusplus
+
+// CPL requires C++20 or later.
+#if !(__cplusplus >= 201703L)
+#pragma message(__FILE__ "(" CPL_STRINGIZE(__LINE__) "): CPL: Unexpected compiler, expected C++20 compiler")
+#error Error in C++ CPL usage; Unsupported compiler.
+#endif  // !(__cplusplus >= 201703L)
+
+#define CPL_BEGIN namespace cpl {
+#define CPL_END }
+
+#endif  // CORE_H_
