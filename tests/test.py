fe8a2f42c95ed67d99928afc486199b7a72d44d9
Brandon <pacewicbrandon@gmail.com>
2022-08-27 19:45:08 -0700

    Update to test framework

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,32 @@
 # Copyright (c) Brandon Pacewic
 # SPDX-License-Identifier: MIT
 
+import mimetypes
 import os
 import sys
 
+
+TEST_OUTPUT_DIR = 'build/tests'
+
+def is_binary(path):
+    return mimetypes.guess_type(path)[0] is not None
+
+
 def main(cmake_source_dir: str) -> None:
-    print("the test ran")
-    print(cmake_source_dir)
+    os.chdir(f'{cmake_source_dir}/{TEST_OUTPUT_DIR}')
+
+    for file in os.listdir('.'):
+        if not is_binary(file):
+            continue
+
+        print(f'Running test {file}')
+        result = os.system(f'./{file}')
+
+        if result != 0:
+            print(f'Test {file} failed')
+            sys.exit(1)
+
+    print('All tests passed')
 
 
 if __name__ == '__main__':
