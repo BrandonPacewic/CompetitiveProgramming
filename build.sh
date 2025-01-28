7ec7e36c517a09b58054f58141f8468a4c752e86
BrandonPacewic <92102436+BrandonPacewic@users.noreply.github.com>
2025-01-27 23:58:49 -0800

    Tests working in pure cmake with ctest

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 # Copyright (c) Brandon Pacewic
 # SPDX-License-Identifier: MIT
 
-if [ ! -d "build" ]; then
-    mkdir build
+if [ -d "build" ]; then
+    rm -rf build/
 fi
 
+mkdir build/
 cd build/
 cmake -G "Unix Makefiles" ../
 make
+ctest --output-on-failure
