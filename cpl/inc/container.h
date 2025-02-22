a335dd407dd335b818b8b116b9eb25504d5ae1f0
BrandonPacewic <92102436+BrandonPacewic@users.noreply.github.com>
2025-02-22 12:56:17 -0800

    Refactor template params for new standard

--- cpl/inc/container.h
+++ cpl/inc/container.h
@@ -11,33 +11,30 @@
 #include <algorithm>
 #include <cstdint>
 #include <iostream>
-#include <set>
 #include <type_traits>
-#include <unordered_set>
 #include <vector>
 
 CPL_BEGIN
 
 template <typename... Ts>
-struct is_container_helper {};
+struct _Is_container_helper {};
 
 template <typename T, typename = void>
-struct is_container_internal : std::false_type {};
+struct _Is_container : std::false_type {};
 
 template <typename T>
-struct is_container_internal<T, std::conditional_t<false,
-                                    is_container_helper<typename T::value_type, decltype(std::declval<T>().begin()),
-                                        decltype(std::declval<T>().end())>,
-                                    void>> : std::true_type {};
+struct _Is_container<T, std::conditional_t<false,
+                            _Is_container_helper<typename T::value_type, decltype(std::declval<T>().begin()),
+                                decltype(std::declval<T>().end())>,
+                            void>> : std::true_type {};
 
 template <typename T>
-constexpr bool is_container = is_container_internal<T>::value;
+constexpr bool is_container = _Is_container<T>::value;
 
 #define CPL_IS_CONTAINER(T) static_assert(is_container<T>, "Templated parameter is not a valid container.")
 
-template <typename ForwardIterator1, typename ForwardIterator2, typename OutputIterator>
-OutputIterator alternating_insertion(ForwardIterator1 first1, ForwardIterator1 last1, ForwardIterator2 first2,
-    ForwardIterator2 last2, OutputIterator result) {
+template <class FwdIt1, class FwdIt2, class OutIt>
+OutIt alternating_insertion(FwdIt1 first1, FwdIt1 last1, FwdIt2 first2, FwdIt2 last2, OutIt result) {
     while (first1 != last1 && first2 != last2) {
         *result++ = *first1++;
         *result++ = *first2++;
@@ -52,29 +49,26 @@ OutputIterator alternating_insertion(ForwardIterator1 first1, ForwardIterator1 l
     return result;
 }
 
-template <typename InputContainer1, typename InputContainer2, typename OutputContainer>
-OutputContainer alternating_insertion(
-    const InputContainer1& input1, const InputContainer2& input2, OutputContainer output) {
+template <class InCont1, class InCont2, class OutCont>
+[[nodiscard]] OutCont alternating_insertion(const InCont1& input1, const InCont2& input2, OutCont output) {
 #if CPL
-    CPL_IS_CONTAINER(InputContainer1);
-    CPL_IS_CONTAINER(InputContainer2);
-    CPL_IS_CONTAINER(OutputContainer);
+    CPL_IS_CONTAINER(InCont1);
+    CPL_IS_CONTAINER(InCont2);
+    CPL_IS_CONTAINER(OutCont);
 #endif // CPL
-    return alternating_insertion(input1.begin(), input1.end(), input2.begin(), input2.end(), output.begin());
+    return alternating_insertion(
+        input1.begin(), input1.end(), input2.begin(), input2.end(), std::back_inserter(output));
 }
 
-template <typename ForwardIterator,
-    typename BaseIteratorType = typename std::iterator_traits<ForwardIterator>::value_type>
-[[nodiscard]] std::vector<std::pair<BaseIteratorType, uint16_t>> run_length_encoding(
-    ForwardIterator first, ForwardIterator last) {
-    std::vector<std::pair<BaseIteratorType, uint16_t>> encoding;
-
+template <class FwdIt, class Ty = typename std::iterator_traits<FwdIt>::value_type>
+[[nodiscard]] std::vector<std::pair<Ty, uint16_t>> run_length_encoding(FwdIt first, FwdIt last) {
+    std::vector<std::pair<Ty, uint16_t>> encoding;
     if (first == last) {
         return encoding;
     }
 
-    BaseIteratorType previous_item = *first;
-    uint16_t         current_count = 0;
+    Ty       previous_item = *first;
+    uint16_t current_count = 0;
 
     for (; first != last; ++first) {
         if (*first == previous_item) {
@@ -96,48 +90,18 @@ template <typename ForwardIterator,
     return encoding;
 }
 
-template <typename T_container, typename ContainerValueType = typename T_container::value_type>
-[[nodiscard]] std::vector<std::pair<ContainerValueType, uint16_t>> run_length_encoding(const T_container& container) {
+template <class Cont, class Ty = typename Cont::value_type>
+[[nodiscard]] std::vector<std::pair<Ty, uint16_t>> run_length_encoding(const Cont& container) {
 #if CPL
-    CPL_IS_CONTAINER(T_container);
+    CPL_IS_CONTAINER(Cont);
 #endif // CPL
     return run_length_encoding(container.begin(), container.end());
 }
 
-template <typename ForwardIterator,
-    typename BaseIteratorType = typename std::iterator_traits<ForwardIterator>::value_type>
-[[nodiscard]] std::set<BaseIteratorType> to_set(ForwardIterator first, ForwardIterator last) {
-    std::set<BaseIteratorType> set_obj(first, last);
-    return set_obj;
-}
-
-template <typename T_container, typename ContainerValueType = typename T_container::value_type>
-[[nodiscard]] std::set<ContainerValueType> to_set(const T_container& container) {
-#if CPL
-    CPL_IS_CONTAINER(T_container);
-#endif // CPL
-    return to_set(container.begin(), container.end());
-}
-
-template <typename ForwardIterator,
-    typename BaseIteratorType = typename std::iterator_traits<ForwardIterator>::value_type>
-[[nodiscard]] std::unordered_set<BaseIteratorType> to_unordered_set(ForwardIterator first, ForwardIterator last) {
-    std::unordered_set<BaseIteratorType> set_obj(first, last);
-    return set_obj;
-}
-
-template <typename T_container, typename ContainerValueType = typename T_container::value_type>
-[[nodiscard]] std::unordered_set<ContainerValueType> to_unordered_set(const T_container& container) {
-#if CPL
-    CPL_IS_CONTAINER(T_container);
-#endif // CPL
-    return to_unordered_set(container.begin(), container.end());
-}
-
-template <typename ForwardIterator>
-ForwardIterator merge_sort(ForwardIterator first, ForwardIterator last) {
+template <class FwdIt>
+FwdIt merge_sort(FwdIt first, FwdIt last) {
     if (std::distance(first, last) > 1) {
-        ForwardIterator middle = first + std::distance(first, last) / 2;
+        FwdIt middle = first + std::distance(first, last) / 2;
         merge_sort(first, middle);
         merge_sort(middle, last);
         std::inplace_merge(first, middle, last);
@@ -146,30 +110,30 @@ ForwardIterator merge_sort(ForwardIterator first, ForwardIterator last) {
     return first;
 }
 
-template <typename T_container>
-T_container merge_sort(T_container& container) {
+template <class Cont>
+Cont merge_sort(Cont& container) {
 #if CPL
-    CPL_IS_CONTAINER(T_container);
+    CPL_IS_CONTAINER(Cont);
 #endif // CPL
 
     merge_sort(container.begin(), container.end());
     return container;
 }
 
-template <typename ForwardIterator>
-ForwardIterator reverse_sort(ForwardIterator first, ForwardIterator last) {
+template <class FwdIt>
+FwdIt reverse_sort(FwdIt first, FwdIt last) {
     for (; first != last; ++first) {
-        ForwardIterator min = std::min_element(first, last);
+        FwdIt min = std::min_element(first, last);
         std::reverse(first, min + 1);
     }
 
     return first;
 }
 
-template <typename T_container>
-T_container reverse_sort(T_container& container) {
+template <class Cont>
+Cont reverse_sort(Cont& container) {
 #if CPL
-    CPL_IS_CONTAINER(T_container);
+    CPL_IS_CONTAINER(Cont);
 #endif // CPL
 
     reverse_sort(container.begin(), container.end());
