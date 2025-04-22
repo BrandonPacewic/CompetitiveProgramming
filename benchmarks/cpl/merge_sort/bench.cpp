380a80ce791e41f06ee41227a57c545396b84c5c
BrandonPacewic <92102436+BrandonPacewic@users.noreply.github.com>
2025-04-22 12:26:13 -0700

    feat(bench): add merge sort benchmark

---
+++ benchmarks/cpl/merge_sort/bench.cpp
@@ -0,0 +1,40 @@
+// Copyright (c) Brandon Pacewic
+// SPDX-License-Identifier: MIT
+
+#include <algorithm>
+#include <benchmark/benchmark.h>
+#include <cstdint>
+#include <random>
+#include <vector>
+
+#include "container.h"
+
+using namespace std;
+using namespace cpl;
+
+template <class T>
+void bench(benchmark::State& state) {
+    mt19937 gen(96337);
+
+    const size_t size = static_cast<size_t>(state.range(0));
+
+    vector<T> input(size);
+
+    uniform_int_distribution<T> dist(numeric_limits<T>::min(), numeric_limits<T>::max());
+    ranges::generate(input, [&] { return dist(gen); });
+
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(input);
+        benchmark::DoNotOptimize(merge_sort(input));
+    }
+}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wignored-attributes"
+BENCHMARK(bench<int8_t>)->Range(8, 8 << 10);
+BENCHMARK(bench<int16_t>)->Range(8, 8 << 10);
+BENCHMARK(bench<int32_t>)->Range(8, 8 << 10);
+BENCHMARK(bench<int64_t>)->Range(8, 8 << 10);
+#pragma GCC diagnostic pop
+
+BENCHMARK_MAIN();
